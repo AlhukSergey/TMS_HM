570dcc4f0ea7d072e3e5743c200f9a12a78c0700
alshuk <alshuk73@gmail.com>
2023-02-02 21:20:25 +0300

    Initial commit

---
+++ src/by/teachmeskills/homeworks/hm_10022023/Calculator.java
@@ -0,0 +1,10 @@
+package by.teachmeskills.homeworks.hm_10022023;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int operand1 = 10;
+        int operand2 = 15;
+        int sum = operand1 + operand2;
+        System.out.println("Сумма двух чисел равна: " + sum);
+    }
+}
