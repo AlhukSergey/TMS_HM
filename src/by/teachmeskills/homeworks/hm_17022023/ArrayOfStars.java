8e25e153a90fd0047e1222b70444db8322330cbd
AlhukSergey <alshuk118@gmail.com>
2023-02-09 21:22:51 +0300

    Finished third task (Array of stars);

---
+++ src/by/teachmeskills/homeworks/hm_17022023/ArrayOfStars.java
@@ -0,0 +1,12 @@
+package by.teachmeskills.homeworks.hm_17022023;
+
+public class ArrayOfStars {
+    public static void main(String[] args) {
+        for(int i = 0; i < 4; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
