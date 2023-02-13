f5c60c3f28c2ad819f51ea0757d6b847837a1cee
AlhukSergey <alshuk118@gmail.com>
2023-02-13 19:51:20 +0300

    The seventh task. Used class Random.

--- src/by/teachmeskills/homeworks/hm_17022023/JaggedArray.java
+++ src/by/teachmeskills/homeworks/hm_17022023/JaggedArray.java
@@ -1,15 +1,20 @@
 package by.teachmeskills.homeworks.hm_17022023;
 
+import java.util.Random;
+
 public class JaggedArray {
     public static void main(String[] args) {
+        final Random randomLength = new Random();
+        final Random randomNumberInArray = new Random();
+
         int[][] array = new int[2][];
 
-        array[0] = new int[(int) Math.round(Math.random() * 10)];
-        array[1] = new int[(int) Math.round(Math.random() * 10)];
+        array[0] = new int[randomLength.nextInt(100)];
+        array[1] = new int[randomLength.nextInt(100)];
 
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
-                array[i][j] = (int) Math.round(Math.random() * 21 - 10);
+                array[i][j] = randomNumberInArray.nextInt();
                 System.out.print(array[i][j] + " ");
             }
             System.out.println();
