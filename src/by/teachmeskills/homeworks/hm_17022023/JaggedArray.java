f77c4f576083970d4af699e551d409b4b2f8d7a7
AlhukSergey <alshuk118@gmail.com>
2023-02-15 18:58:03 +0300

    Seventh task. Edited array.

--- src/by/teachmeskills/homeworks/hm_17022023/JaggedArray.java
+++ src/by/teachmeskills/homeworks/hm_17022023/JaggedArray.java
@@ -5,16 +5,17 @@ import java.util.Random;
 public class JaggedArray {
     public static void main(String[] args) {
         final Random randomLinesNumber = new Random();
-        final Random randomColumnsNUmber = new Random();
+        final Random randomColumnsNumber = new Random();
         final Random randomNumberInArray = new Random();
 
-        int[][] array = new int[randomLinesNumber.nextInt(15)][randomColumnsNUmber.nextInt(15)];
+        int[][] array = new int[randomLinesNumber.nextInt(10)][];
 
 
         for (int i = 0; i < array.length; i++) {
+            array[i] = new int[randomColumnsNumber.nextInt(15)];
             for (int j = 0; j < array[i].length; j++) {
-                array[i][j] = randomNumberInArray.nextInt();
-                System.out.print(array[i][j] + " ");
+                array[i][j] = randomNumberInArray.nextInt(10);
+                System.out.print(array[i][j]);
             }
             System.out.println();
         }
