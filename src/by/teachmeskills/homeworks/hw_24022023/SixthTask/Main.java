6f8120f7cd4331131e46c5ad02c916846dfe48f7
AlhukSergey <alshuk118@gmail.com>
2023-02-18 13:30:30 +0300

    Sixth task (Computer).

---
+++ src/by/teachmeskills/homeworks/hw_24022023/SixthTask/Main.java
@@ -0,0 +1,11 @@
+package by.teachmeskills.homeworks.hw_24022023.SixthTask;
+
+public class Main {
+    public static void main(String[] args) {
+        Computer computer = new Computer("MISC", "4 gigabyte", "1 terabyte");
+        computer.showInfo();
+        computer.turnOn();
+        computer.turnOff();
+    }
+}
+
