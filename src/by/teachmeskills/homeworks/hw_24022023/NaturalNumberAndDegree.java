92f054fe580c55bfa557a409abcba467b8a61dba
AlhukSergey <alshuk118@gmail.com>
2023-02-19 08:23:55 +0300

    Corrected fifth task (checking degree).

--- src/by/teachmeskills/homeworks/hw_24022023/NaturalNumberAndDegree.java
+++ src/by/teachmeskills/homeworks/hw_24022023/NaturalNumberAndDegree.java
@@ -2,15 +2,14 @@ package by.teachmeskills.homeworks.hw_24022023;
 
 public class NaturalNumberAndDegree {
     public static void main(String[] args) {
-        int n = checkDegree2(9);
+        int n = checkDegree(9);
         System.out.println(n == 1 ? "YES" : "NO");
-
     }
 
-    private static int checkDegree2(int n) {
+    private static int checkDegree(int n) {
         if (n % 2 == 1) {
             return n;
         }
-        return checkDegree2(n / 2);
+        return checkDegree(n / 2);
     }
 }
