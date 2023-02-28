90026b75790c70592e99682772e2f3f312c6978e
AlhukSergey <alshuk118@gmail.com>
2023-02-28 17:06:50 +0300

    Edited fourth task (OOP). Final variables to uppercase, autogenerate id, class Validator as a local class.

--- src/by/teachmeskills/homeworks/hm_03032023/OOP/Bank/BankCard.java
+++ src/by/teachmeskills/homeworks/hm_03032023/OOP/Bank/BankCard.java
@@ -2,53 +2,57 @@ package by.teachmeskills.homeworks.hm_03032023.OOP.Bank;
 
 public class BankCard {
     private static final String[] owners = {"Ivan", "Sergey", "Roman"};
-    private final String id;
-    private final String number;
-    private final String type;
-    private final String cardOwner;
-    private final String validity;
-    private final String cvv;
+    private static int incID = 0;
+    private final int ID;
+    private final String NUMBER;
+    private final String TYPE;
+    private final String CARDOWNER;
+    private final String VALIDITY;
+    private final String CVV;
 
-    public BankCard(String id, String number, String type, String cardOwner, String validity, String cvv) {
-        this.id = id;
-        this.number = number;
-        this.type = type;
-        this.cardOwner = cardOwner;
-        this.validity = validity;
-        this.cvv = cvv;
-        System.out.println((new Validator().checkCVV() &&
-                new Validator().checkOwner() &&
-                new Validator().checkDigitsNumber() &&
-                new Validator().checkValidity() &&
-                new Validator().checkType()) ? "The card is valid." : "The card isn't valid.");
+    public BankCard(String NUMBER, String TYPE, String CARDOWNER, String VALIDITY, String CVV) {
+        this.ID = incID++;
+        this.NUMBER = NUMBER;
+        this.TYPE = TYPE;
+        this.CARDOWNER = CARDOWNER;
+        this.VALIDITY = VALIDITY;
+        this.CVV = CVV;
     }
 
-    private class Validator {
-        private boolean checkDigitsNumber() {
-            return number.length() == 16;
-        }
-
-        private boolean checkType() {
-            return (type.equals("MasterCard") || type.equals("Visa"));
-        }
-
-        private boolean checkOwner() {
-            boolean flag = false;
-            for (String owner : owners) {
-                if (owner.equals(cardOwner)) {
-                    flag = true;
-                    break;
-                }
+    public void checkValid() {
+        class Validator {
+            private boolean checkDigitsNumber() {
+                return NUMBER.length() == 16;
             }
-            return flag;
-        }
 
-        private boolean checkValidity() {
-            return validity.equals("20.02");
-        }
+            private boolean checkType() {
+                return (TYPE.equals("MasterCard") || TYPE.equals("Visa"));
+            }
 
-        private boolean checkCVV() {
-            return cvv.equals("123");
+            private boolean checkOwner() {
+                boolean flag = false;
+                for (String owner : owners) {
+                    if (owner.equals(CARDOWNER)) {
+                        flag = true;
+                        break;
+                    }
+                }
+                return flag;
+            }
+
+            private boolean checkValidity() {
+                return VALIDITY.equals("20.02");
+            }
+
+            private boolean checkCVV() {
+                return CVV.equals("123");
+            }
         }
+        Validator validator = new Validator();
+        System.out.println((validator.checkCVV() &&
+                validator.checkOwner() &&
+                validator.checkDigitsNumber() &&
+                validator.checkValidity() &&
+                validator.checkType()) ? "The card is valid." : "The card isn't valid.");
     }
 }
--- src/by/teachmeskills/homeworks/hm_03032023/OOP/Bank/Run.java
+++ src/by/teachmeskills/homeworks/hm_03032023/OOP/Bank/Run.java
@@ -2,11 +2,7 @@ package by.teachmeskills.homeworks.hm_03032023.OOP.Bank;
 
 public class Run {
     public static void main(String[] args) {
-        BankCard card = new BankCard("123",
-                "1234123412341234",
-                "MasterCard",
-                "Ivan",
-                "20.02",
-                "123");
+        BankCard card = new BankCard("1234123412341234", "MasterCard", "Ivan", "20.02", "123");
+        card.checkValid();
     }
 }
