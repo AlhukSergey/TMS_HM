5bf758387d9c6480c3bcd40a6a2a7ab7ee45d074
AlhukSergey <alshuk118@gmail.com>
2023-03-20 21:39:18 +0300

    Edited first task (Renamed the archive, deleted archives).

--- src/by/teachmeskills/homeworks/hm_31032023/zip/Main.java
+++ src/by/teachmeskills/homeworks/hm_31032023/zip/Main.java
@@ -1,9 +1,14 @@
 package by.teachmeskills.homeworks.hm_31032023.zip;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Comparator;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class Main {
@@ -14,15 +19,42 @@ public class Main {
     private static final String FILE2_NAME = "file2.txt";
     private static final String FILE3_NAME = "file3.txt";
     private static final String ARCHIVE_PATH = "D:\\Study\\TMS\\hm_31032023\\archive.zip";
+    private static final String NEW_ARCHIVE_PATH = "D:\\Study\\TMS\\hm_31032023\\archive1.zip";
 
     public static void main(String[] args) {
         try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(ARCHIVE_PATH))) {
             putInArchive(zos, FILE1_PATH, FILE1_NAME);
             putInArchive(zos, FILE2_PATH, FILE2_NAME);
             putInArchive(zos, FILE3_PATH, FILE3_NAME);
+            System.out.println("The files was successfully archived!");
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+
+        File file = new File(ARCHIVE_PATH);
+        System.out.println(file.getName());
+        File newFile = new File(NEW_ARCHIVE_PATH);
+        if (file.renameTo(newFile)) {
+            System.out.println("The file successfully renamed.");
+        } else {
+            System.out.println("The file wasn't renamed.");
+        }
+        System.out.println(newFile.getName());
+
+        try (ZipInputStream zis = new ZipInputStream(new FileInputStream(NEW_ARCHIVE_PATH))) {
+            ZipEntry zipEntry;
+            String archiveName;
+
+            while ((zipEntry = zis.getNextEntry()) != null) {
+                archiveName = zipEntry.getName();
+                System.out.printf("File name : %s. \n", archiveName);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        deleteDirectory(Path.of(NEW_ARCHIVE_PATH));
+        deleteDirectory(Path.of(ARCHIVE_PATH));
     }
 
     private static void putInArchive(ZipOutputStream zos, String filePath, String fileName) {
@@ -33,6 +65,14 @@ public class Main {
             fis.read(buffer);
             zos.write(buffer);
             zos.closeEntry();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void deleteDirectory(Path directory) {
+        try {
+            Files.walk(directory).sorted(Comparator.reverseOrder()).map(Path::toFile).forEach(File::delete);
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
