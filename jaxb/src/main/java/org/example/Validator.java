2382ee650bd356661c72592ce738ddaa3a7d9e63
AlhukSergey <alshuk118@gmail.com>
2023-05-10 16:53:58 +0300

    Cleaned code (regex as a local variable);

--- jaxb/src/main/java/org/example/Validator.java
+++ jaxb/src/main/java/org/example/Validator.java
@@ -6,11 +6,16 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Validator {
+    private static String REGEX_NAME = "[A-Z][a-z]+(\\s[A-Z][a-z]+){2}";
+    private static String REGEX_POSITION = "[A-Z][a-z\\s-]+";
+    private static String REGEX_DEPARTMENT = "[\\d\\sA-Za-z-]+";
+    private static String REGEX_EXPERIENCE = "[\\d]+[\\syears]";
+
     private Validator() {
     }
 
     public static void checkName(String name) throws ValidationException {
-        Pattern pattern = Pattern.compile("[A-Z][a-z]+(\\s[A-Z][a-z]+){2}");
+        Pattern pattern = Pattern.compile(REGEX_NAME);
         Matcher matcher = pattern.matcher(name);
         if (!matcher.matches()) {
             throw new ValidationException("The name isn't valid!");
@@ -18,7 +23,7 @@ public class Validator {
     }
 
     public static void checkPosition(String position) throws ValidationException {
-        Pattern pattern = Pattern.compile("[A-Z][a-z\\s-]+");
+        Pattern pattern = Pattern.compile(REGEX_POSITION);
         Matcher matcher = pattern.matcher(position);
         if (!matcher.matches()) {
             throw new ValidationException("The position isn't valid!");
@@ -26,7 +31,7 @@ public class Validator {
     }
 
     public static void checkDepartment(String department) throws ValidationException {
-        Pattern pattern = Pattern.compile("[\\d\\sA-Za-z-]+");
+        Pattern pattern = Pattern.compile(REGEX_DEPARTMENT);
         Matcher matcher = pattern.matcher(department);
         if (!matcher.matches()) {
             throw new ValidationException("The department isn't valid!");
@@ -34,7 +39,7 @@ public class Validator {
     }
 
     public static void checkExperience(String experience) throws ValidationException {
-        Pattern pattern = Pattern.compile("[\\d]+[\\syears]");
+        Pattern pattern = Pattern.compile(REGEX_EXPERIENCE);
         Matcher matcher = pattern.matcher(experience);
         if (!matcher.matches()) {
             throw new ValidationException("The experience isn't valid!");
